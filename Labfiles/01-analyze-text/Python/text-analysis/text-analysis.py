69948d35345efcaa0380e54c3a20bfa6c15d9729
DK <daniasadiaa@gmail.com>
2026-06-10 21:12:57 +0200

    Speech solution based on the Use speech-capable generative AI models lab.

--- Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
+++ Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
@@ -1,8 +1,7 @@
 from dotenv import load_dotenv
 import os
-
-# Import namespaces
-
+from azure.identity import DefaultAzureCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 
 def main():
     try:
@@ -12,12 +11,11 @@ def main():
         # Get Configuration Settings
         load_dotenv()
         foundry_endpoint = os.getenv('FOUNDRY_ENDPOINT')
+        
 
+        credential = DefaultAzureCredential(exclude_managed_identity_credential=True)
+        ai_client = TextAnalyticsClient(foundry_endpoint, credential)
 
-        # Create client using endpoint
-
-
-        # Analyze each text file in the reviews folder
         reviews_folder = 'reviews'
         for file_name in os.listdir(reviews_folder):
             # Read the file contents
@@ -25,18 +23,19 @@ def main():
             text = open(os.path.join(reviews_folder, file_name), encoding='utf8').read()
             print('\n' + text)
 
-            # Get language
-
-
-
-            # Get entities
-
-
-
-            # Get PII
-
-
-
+        detected_language = ai_client.detect_language([text])[0]
+        print("\nLanguage: {}".format(detected_language.primary_language.name))
+        enteties = ai_client.recognize_entities([text])[0].entities
+        if(len(enteties) > 0):
+            print("\nEnteties")
+            for entity in enteties:
+             print("\t{} ({})".format(entity.text, entity.category))
+        pii_entities = ai_client.recognize_pii_entities([text])[0]
+        if (len(pii_entities.entities) > 0):
+            print("\nPIIEntities:")
+            for pii_entity in pii_entities.entities:
+                print("\t{} ({})".format(pii_entity.text, pii_entity.category))
+            print("Redacted Text:\n{}".format(pii_entities.redacted_text))
     except Exception as ex:
         print(ex)
 
--- Labfiles/03-gen-ai-speech/Python/generate-speech/generate-speech.py
+++ Labfiles/03-gen-ai-speech/Python/generate-speech/generate-speech.py
@@ -2,10 +2,11 @@ import os
 from pathlib import Path
 from playsound3 import playsound
 from dotenv import load_dotenv
-
+import base64
+from urllib.parse import urlparse, urlunparse
 # Import namespaces
-
-
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+from openai import AzureOpenAI
 
 def main():
     try:
@@ -14,21 +15,32 @@ def main():
 
         # Get Configuration Settings
         load_dotenv()
-        endpoint=os.getenv("MODEL_ENDPOINT")
-        model_deployment=os.getenv("MODEL_NAME")
+        endpoint = os.getenv("MODEL_ENDPOINT")
+        model_deployment = os.getenv("MODEL_NAME")
         speech_file_path = Path(__file__).parent / "speech.mp3"
 
-
         # Create the Azure OpenAI client
-        
-
+        token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://ai.azure.com/.default")
+        client = AzureOpenAI(azure_endpoint=endpoint, azure_ad_token_provider=token_provider, api_version="2025-03-01-preview")
 
         # Generate speech and save to file
-        
+        response = client.chat.completions.create(
+            model=model_deployment,
+            modalities=["text", "audio"],
+            audio={"voice": "echo", "format": "mp3"},
+            messages=[
+                {"role": "user", "content": "This is an implementation of the lab for Use speech-capable generative AI models."}
+            ],
+        )
+        res = response.choices[0].message.audio.data
+        audio = base64.b64decode(res)
+        with open(speech_file_path, "wb") as audio_file:
+            audio_file.write(audio)
 
+        print(f"Saved generated speech to: {speech_file_path}")
 
         # Play the generated speech file
-        playsound(speech_file_path)
+       # playsound(speech_file_path)
 
     except Exception as ex:
         print(ex)
